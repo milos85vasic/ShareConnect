a5aa8cd168d03d85d093fc5af8a12af8469aee8c
Milos Vasic <i@mvasic.ru>
2025-10-08 21:17:01 +0300

    ShareConnect - Min and compile versions set.

--- ProfileSync/build.gradle.kts
+++ ProfileSync/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
     namespace = "com.shareconnect.profilesync"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 36
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
